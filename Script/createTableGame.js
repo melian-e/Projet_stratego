3d2176b3a433bbe200880ac42dcaebc01913fd37
FloraCousin <60597446+FloraCousin@users.noreply.github.com>
2021-04-05 14:45:43 +0200

    Add files via upload

---
+++ Script/createTableGame.js
@@ -0,0 +1,123 @@
+
+function preparation(color){
+    let table = document.createElement("table");
+    table.id = 'game-table';
+
+    for(let i = 0; i < 10; i++){
+        table.appendChild(createLine((i < 6) ? false : true));
+    }
+
+    document.getElementById('game-board').appendChild(table);
+    counter(2);
+    createLake();
+    buton();
+    stock(color);
+}
+
+function createLake(){
+    let td = document.getElementsByClassName("case");
+    
+    for(let x = 4; x<6; x++){
+        for(let y = 0; y < 5; y+=4){
+            for(let i = y+2; i < y+4; i++){
+                let lake = td[x*10+i];
+
+                let lakeNum = (y == 0) ? 1 : 2;
+                let caseNum = (x == 4) ? ((i % 2 == 0) ? 1 : 2) : ((i % 2 == 0) ? 3 : 4);
+                
+                lake.classList.add('lake');
+                lake.classList.add("l"+lakeNum+"-"+caseNum);
+            }
+        }
+    }
+}
+
+function createLine(move){
+    let line = document.createElement('tr');
+
+    for(let i = 0; i < 10; i++){
+        let td = document.createElement('td');
+
+        td.classList.add('case');
+        
+        if(move == true){
+            td.addEventListener("drop", drop);
+            td.addEventListener("dragenter", dragEnter);
+            td.addEventListener("dragleave", dragLeave);
+            td.addEventListener("dragover", allowDrop);
+        }
+
+        line.appendChild(td);
+    }
+    return line;
+}
+
+function buton(){
+    let resetBtn = document.getElementById("reset");
+    let randomBtn = document.getElementById("random");
+    let startBtn = document.getElementById("start");
+
+    resetBtn.addEventListener("click", reset);
+    randomBtn.addEventListener("click",randGrid);
+    startBtn.addEventListener("click", start);
+}
+
+
+function stock(color){
+    let stock = document.createElement("table");
+    stock.id = "pionStock";
+
+    for(let x = 3; x > 0; x--){
+        stock.appendChild(createStock(4*x-2,color));
+    }
+
+    document.getElementById("pions").appendChild(stock);
+    
+}
+
+function createStock(i, color){
+    let tr = document.createElement("tr");
+
+    for (let x = 0; x < 4; x++){
+        let td = document.createElement("td");
+        td.classList.add("stock");
+        td.id = "s";
+        td.id += (i != -1) ? "p"+i : "b";
+        let j;
+        switch(i){
+            case 10: j = 1; break;
+            case 8: j = 2; break;
+            case 9: j = 1; break;
+            case 7: j = 3; break;
+            case 6: j = 4; break;
+            case 5: j = 4; break;
+            case 4: j = 4; break;
+            case 3: j = 5; break;
+            case 2: j = 8; break;
+            case 1: j = 1; break;
+            case 0: j = 1; break;
+            case -1: j = 6; break;
+        }
+
+        for(y = 1; y < j+1; y++){
+            let pion = document.createElement("div");
+            
+            let className = (i != -1) ? "p" +i : "bomb";
+            pion.classList.add("dot");
+            pion.classList.add(className);
+            pion.classList.add(color);
+
+            let name = (i != -1) ? "p"+i : "b";
+            name += (j != 1) ? "-"+y : "";
+            pion.id = name;
+            
+            pion.draggable = true;
+            pion.addEventListener("dragstart", drag);
+            td.appendChild(pion);
+        }
+        i--;
+        tr.appendChild(td);
+    }
+
+    return tr;
+}
---
+++ Script/interaction.js
@@ -0,0 +1,130 @@
+socket.emit('preparation');
+
+socket.on('end', message =>{
+    let td = document.getElementsByClassName("case");
+    
+    removeClicks();
+    for(let x = 0; x < td.length; x++){
+        td[x].style.backgroundColor = "transparent";
+        td[x].removeEventListener("click", moveOnClick);
+        if(td[x].firstElementChild != undefined){
+            td[x].firstElementChild.removeEventListener("dragstart", dragGame);
+        }
+    }
+
+    console.log(message);
+});
+
+socket.on('preparation', color => preparation(color));
+socket.on('display', (table, color, turn) => display(table, color, turn));
+
+socket.on('new-spectator', (grid,time) => {
+
+    let table = document.createElement("table");
+    table.id = 'game-table';
+
+    for(let i = 0; i < 10; i++){
+        table.appendChild(createLine((i < 6) ? false : true));
+    }
+
+    document.getElementById('game-board').appendChild(table);
+    createLake();
+
+    document.getElementById("reset").remove();
+    document.getElementById("random").remove();
+    document.getElementById("start").remove();
+
+    time = (+new Date) - time;
+
+    if(grid.every(row => row.every(tab => tab[0] > 15))){
+        
+        let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+        counter(2 - (minutes + ((seconds * 0.5)/30)));
+    }
+    else{
+        document.getElementById("timer").style.display = "none";     
+
+        let pion = document.getElementById("pions");
+        let clock = document.createElement("div");
+        
+        clock.id = "chrono";
+        pion.appendChild(clock);
+
+        chrono(time);
+    }
+
+    display(grid, 'none', false);
+});
+
+let dragInProgress = false;
+
+socket.on('start', () => {
+    let td = document.getElementsByClassName("case");
+
+    for(let i = 0; i < td.length; i++){
+        td[i].addEventListener("drop", event =>{
+            if(dragInProgress == true){
+                dragInProgress = false;
+                dropGame(event);
+            }
+        });
+        td[i].addEventListener("dragenter", wrapperEnter);
+        td[i].addEventListener("dragleave", wrapperLeave);
+        td[i].addEventListener("dragover", wrapperOver);
+    }
+
+    document.getElementById("timer").style.display = "none";
+    let clock = document.createElement("div");
+    let pion = document.getElementById("pions");
+
+    clock.id = "chrono";
+    pion.appendChild(clock);
+    chrono(0);
+});
+
+function getCase(pion){
+    let positionPion;
+    let idPion = pion.id;
+
+    if(idPion.search("p") != -1){
+        let pos = idPion.search("p");
+        let num = Number(idPion[pos+1]);
+
+        positionPion = (idPion.length == 2 || idPion[pos+2] == '-') ? 10 - num : 0;
+    }
+    else positionPion = 11;
+
+    return positionPion;
+}
+
+function randCase(pion, grid){
+    let lin, col, nb;
+    do{
+        lin = Math.floor(Math.random() * 10);
+        col = Math.floor(Math.random() * 4) + 6;
+        nb = col * 10 + lin;
+    } while(grid[nb].hasChildNodes() == true);
+    grid[nb].appendChild(pion);
+}
+
+function randGrid(){
+    let pion = document.getElementsByClassName("dot");
+    let grid = document.getElementsByClassName("case");
+    for(let i = 0; i < pion.length; i++){
+        if(pion[i].parentNode.parentNode.parentNode != document.getElementById("game-table")){
+            pion[i].style.width = "90%";
+            pion[i].style.heigth = "100%";
+            pion[i].style.position = "relative";
+            randCase(pion[i], grid);
+        }
+    }
+}
+
+function resetGameBoard(){
+    let grid = document.getElementsByClassName("case");
+    for(let i = 0; i < grid.length; i++){
+        grid[i].style.backgroundColor = "transparent";
+    }
+}
