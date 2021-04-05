d0569ef0f0c9b8bd34b902255eab8f1519c4eb27
LY0718\Administrateur <iemelian.rambeau@student.junia.com>
2021-04-05 20:39:03 +0200

    choicegamemode pour debug avec alexis

--- Front/Script/choiceGamemode.js
+++ Front/Script/choiceGamemode.js
@@ -1,12 +1,13 @@
-let piece1 = document.getElementById("piece1");
-let eclaireur1 = document.getElementById("eclaireur1");
-let bomb1 = document.getElementById("bomb1");
+let form = document.getElementById("form");
 
 form.addEventListener('submit', event => {
+    let piece1 = document.getElementById("piece1");
+    let eclaireur1 = document.getElementById("eclaireur1");
+    let bomb1 = document.getElementById("bomb1");
+
     event.preventDefault();
     console.log(piece1,eclaireur1,bomb1);
     socket.emit("search-game",(piece1 ? true : false),(piece1 ? true : false),(piece1 ? true : false));
-    
 });
 
 
