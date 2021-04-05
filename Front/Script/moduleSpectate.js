8e4ff2a16afa37e4b7fc2e8e1392ebb8cff7d0a2
unknown <alexis.chevet@student.yncrea.fr>
2021-04-05 16:25:31 +0200

    ACT tbleau de choix pour le spectateur et modification des chemins de l'icone dans les header des html

--- Front/Script/moduleSpectate.js
+++ Front/Script/moduleSpectate.js
@@ -12,18 +12,36 @@ mdpButton.addEventListener('click', event=> {
 
 let setPartie = (function(){
     socket.emit('current-games');
-    socket.on('current-games',(table)=>{
-        for (let i = 0; i < table.length; i++) {
-            let row = document.createElement("tr");
 
-            for (let j = 0; j < 3; j++) {
+    socket.on('current-games', table =>{
+        //table = [["alexis","Axel", "2:03"],["IéIé", "Flora", "12:02"]];
+
+        let location = document.getElementById("games");
+
+        if(table.length != 0){
+            let tab = document.createElement("table");
+
+            for (let i = 0; i < table.length; i++) {
+                let row = document.createElement("tr");
                 let cell = document.createElement("td");
-                let cellText = document.createTextNode("cell in row "+i+", column "+j);        
-                cell.appendChild(cellText);
+                let text = document.createTextNode(table[i][0] + " et " + table[i][1] + " jouent depuis " + table[i][2] + "min");
+
+                cell.classList.add("game");
+                cell.addEventListener("click", selectGame);
+                cell.appendChild(text);
                 row.appendChild(cell);
+                /*for (let j = 0; j < 3; j++) {
+                    let cell = document.createElement("td");
+                    let cellText = document.createTextNode("cell in row "+i+", column "+j);        
+                    cell.appendChild(cellText);
+                    row.appendChild(cell);
+                }
+                tableBody.appendChild(row);*/
+                tab.appendChild(row);
             }
-            tableBody.appendChild(row);
+            location.appendChild(tab);
         }
+        else location.innerHTML = "Aucune partie en cours.";
     });
 });
 
@@ -36,6 +54,17 @@ let getPseudo = (function(){
             divPseudo.innerHTML = "Bienvenue " + username + " !";
         }
     });
+});
+
+let selectGame = (function(event){
+    let td = document.getElementsByClassName("game");
+    let i = 0;
+
+    while(td[i] != event.currentTarget){
+        i++;
+    }
+
+    socket.emit("new-spectator", i);
 });
 
 getPseudo();
