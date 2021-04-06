351219d2685f567d7dff7dcd6b7a5226d3387457
unknown <alexis.chevet@student.yncrea.fr>
2021-04-06 21:43:50 +0200

    ACT bdd clean.

--- Back/Bdd/projet_stratego.sql
+++ Back/Bdd/projet_stratego.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 06 avr. 2021 à 17:02
--- Version du serveur :  5.7.28
--- Version de PHP :  5.6.40
+-- Hôte : 127.0.0.1
+-- Généré le :  mar. 06 avr. 2021 à 21:43
+-- Version du serveur :  5.7.17
+-- Version de PHP :  5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,9 +28,8 @@ SET time_zone = "+00:00";
 -- Structure de la table `games`
 --
 
-DROP TABLE IF EXISTS `games`;
-CREATE TABLE IF NOT EXISTS `games` (
-  `id_game` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `games` (
+  `id_game` int(11) NOT NULL,
   `id_winner` int(11) NOT NULL,
   `id_loser` int(11) NOT NULL,
   `score_winner` int(11) NOT NULL,
@@ -38,52 +37,56 @@ CREATE TABLE IF NOT EXISTS `games` (
   `date` text NOT NULL,
   `play_time` text NOT NULL,
   `name_winner` text NOT NULL,
-  `name_loser` text NOT NULL,
-  PRIMARY KEY (`id_game`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+  `name_loser` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `games`
 --
 
 INSERT INTO `games` (`id_game`, `id_winner`, `id_loser`, `score_winner`, `score_loser`, `date`, `play_time`, `name_winner`, `name_loser`) VALUES
-(7, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
-(8, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
-(9, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
-(10, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
-(11, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
-(12, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(12) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(12) NOT NULL,
-  `mmr` int(11) NOT NULL COMMENT 'valeur permettant le classement du joueur',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+(9, 9, 10, 35, -10, '2021-04-05', '14:23', 'alexis', 'axel'),
+(10, 9, 10, 35, -10, '2021-04-05', '15:32', 'alexis', 'axel'),
+(11, 10, 9, 35, -10, '2021-04-05', '19:07', 'axel', 'alexis'),
+(12, 10, 9, 35, -10, '2021-04-05', '13:08', 'axel', 'alexis'),
+(13, 10, 9, 35, -10, '2021-04-05', '5:12', 'axel', 'alexis'),
+(14, 10, 9, 35, -10, '2021-04-06', '10:05', 'axel', 'alexis'),
+(15, 10, 9, 35, -10, '2021-04-06', '25:11', 'axel', 'alexis'),
+(16, 10, 9, 35, -10, '2021-04-06', '30:09', 'axel', 'alexis'),
+(17, 10, 9, 35, -10, '2021-04-06', '41:40', 'axel', 'alexis'),
+(18, 10, 9, 35, -10, '2021-04-06', '40:05', 'axel', 'alexis'),
+(19, 9, 10, 35, -10, '2021-04-06', '57:50', 'alexis', 'axel'),
+(20, 9, 10, 35, -10, '2021-04-06', '34:58', 'alexis', 'axel'),
+(21, 9, 10, 35, -10, '2021-04-06', '34:09', 'alexis', 'axel'),
+(22, 9, 10, 35, -10, '2021-04-06', '43:06', 'alexis', 'axel'),
+(23, 9, 10, 35, -10, '2021-04-06', '34:25', 'alexis', 'axel'),
+(48, 10, 11, 35, -10, '2021-04-06', '55:02', 'axel', 'IéIé'),
+(47, 11, 9, 35, -10, '2021-04-06', '14:01', 'IéIé', 'alexis'),
+(46, 11, 9, 35, -10, '2021-04-06', '30:02', 'IéIé', 'alexis'),
+(45, 11, 10, 35, -10, '2021-04-06', '44:04', 'IéIé', 'axel'),
+(42, 9, 10, 35, -10, '2021-04-06', '21:35', 'alexis', 'axel'),
+(43, 10, 9, 35, -10, '2021-04-06', '20:28', 'axel', 'alexis'),
+(44, 11, 10, 35, -10, '2021-04-06', '13:34', 'IéIé', 'axel');
 
 --
--- Déchargement des données de la table `users`
+-- Index pour les tables déchargées
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `mmr`) VALUES
-(6, 'test', 'test1@test.test', 'test', 200),
-(5, 'test1', 'test@test.test', 'test', 200),
-(7, 'test2', 'test2@test.test', 'test', 200),
-(8, 'test4', 'test4@test.test', 'test', 200),
-(9, 'testtest', 'test4test@test.test', 'test', 200),
-(10, 'testtestest', 'testsetest@test.test', 'test', 200);
-COMMIT;
+--
+-- Index pour la table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id_game`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id_game` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
