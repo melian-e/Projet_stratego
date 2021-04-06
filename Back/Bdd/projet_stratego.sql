29d187d245251c2e88be6b13ec12d8c0a31ba6fd
LY0718\Administrateur <iemelian.rambeau@student.junia.com>
2021-04-06 19:03:10 +0200

    nouvelle bdd mais pas vide

--- Back/Bdd/projet_stratego.sql
+++ Back/Bdd/projet_stratego.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 05 avr. 2021 à 14:55
+-- Généré le :  mar. 06 avr. 2021 à 17:02
 -- Version du serveur :  5.7.28
 -- Version de PHP :  5.6.40
 
@@ -35,10 +35,24 @@ CREATE TABLE IF NOT EXISTS `games` (
   `id_loser` int(11) NOT NULL,
   `score_winner` int(11) NOT NULL,
   `score_loser` int(11) NOT NULL,
-  `date` date NOT NULL,
+  `date` text NOT NULL,
   `play_time` text NOT NULL,
+  `name_winner` text NOT NULL,
+  `name_loser` text NOT NULL,
   PRIMARY KEY (`id_game`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `games`
+--
+
+INSERT INTO `games` (`id_game`, `id_winner`, `id_loser`, `score_winner`, `score_loser`, `date`, `play_time`, `name_winner`, `name_loser`) VALUES
+(7, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
+(8, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
+(9, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
+(10, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
+(11, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest'),
+(12, 10, 9, 35, -10, '2021-04-05', '1:32', 'testtestest', 'testtest');
 
 -- --------------------------------------------------------
 
@@ -56,11 +70,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `users`
 --
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `mmr`) VALUES
+(6, 'test', 'test1@test.test', 'test', 200),
+(5, 'test1', 'test@test.test', 'test', 200),
+(7, 'test2', 'test2@test.test', 'test', 200),
+(8, 'test4', 'test4@test.test', 'test', 200),
+(9, 'testtest', 'test4test@test.test', 'test', 200),
+(10, 'testtestest', 'testsetest@test.test', 'test', 200);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
